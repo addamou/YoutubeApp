6008316df196cf3103708d1b16e89a29f494d5d6
Addamou <adamou.halidou@codeloccol.org>
2020-10-10 13:37:07 +0100

    Updates

---
+++ precache-manifest.2b207662e4e181c88a3fafbd8dfeadcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65804bb3693d479fdc458255827ca2d9",
+    "url": "/YoutubeApp/index.html"
+  },
+  {
+    "revision": "cf18c016c5b79a5844c8",
+    "url": "/YoutubeApp/static/js/2.50d9d2fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/YoutubeApp/static/js/2.50d9d2fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71d772e6ded5025fc69",
+    "url": "/YoutubeApp/static/js/main.2607e945.chunk.js"
+  },
+  {
+    "revision": "e99a0e0c76f7b9bc3e13",
+    "url": "/YoutubeApp/static/js/runtime-main.e049a4e8.js"
+  }
+]);
